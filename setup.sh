bdd3fcd50b141f02fcd2ff2f00ecd9250fac05d1
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2020-03-02 21:00:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 apt update
 apt install git -y
-git clone https://gitee.com/yuqingkj/double_miner.git
+git clone https://github.com/yuqingkj/double_mine.git
 sudo apt-get install nvidia-cuda-toolkit -y
-cd mine_xmr
-chmod 777 ./xmrig
-screen -dmS xmr
-cmd=$"./xmrig";
-screen -x -S xmr -p 0 -X stuff "$cmd"
+cd double_mine
+chmod 777 ./xmrig ./bminer ./mine_grin29.sh
+cd
+screen -dmS grin
+cmd1=$"cd double_mine && ./xmrig";
+screen -x -S xmr -p 0 -X stuff "$cmd1"
 screen -x -S xmr -p 0 -X stuff $'\n'
+screen -dmS grin
+cmd2=$"cd double_mine && ./mine_grin29.sh";
+screen -x -S grin -p 0 -X stuff $'cmd2'
+screen -x -S grin -p 0 -X stuff $'\n'
