1d940a20adc76749a8bf864a99be93ab6a4a0ad8
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2020-03-03 10:18:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,10 @@ git clone https://github.com/yuqingkj/double_mine.git
 sudo apt-get install nvidia-cuda-toolkit -y
 cd double_mine
 chmod 777 ./xmrig ./bminer ./mine_grin29.sh
-cd
-screen -dmS grin
-cmd1=$"cd double_mine && ./xmrig";
-screen -x -S xmr -p 0 -X stuff "$cmd1"
+screen -dmS xmr
+screen -x -S xmr -p 0 -X stuff "./xmrig";
 screen -x -S xmr -p 0 -X stuff $'\n'
 screen -dmS grin
-cmd2=$"cd double_mine && ./mine_grin29.sh";
-screen -x -S grin -p 0 -X stuff $'cmd2'
+screen -x -S grin -p 0 -X stuff "./mine_grin29.sh";
 screen -x -S grin -p 0 -X stuff $'\n'
+screen -ls
