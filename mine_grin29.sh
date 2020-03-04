29325cd0c7adc2ed80e2f8d7366d14db3e4ef01f
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2020-03-04 12:50:11 +0800

    Update mine_grin29.sh

--- mine_grin29.sh
+++ mine_grin29.sh
@@ -2,8 +2,8 @@
 
 # Change the following address to your GRIN addr.
 ADDRESS=yuqingkj@gmail.com
-USERNAME=$ADDRESS%2FBlue
-POOL=grin-eu.sparkpool.com:6666
+USERNAME=$ADDRESS%2FGreen
+POOL=grin.sparkpool.com:6666
 SCHEME=cuckaroo29m
 
 ./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880
